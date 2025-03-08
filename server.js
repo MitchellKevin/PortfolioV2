f890556bebd956344f9c788d7dc4394e5eaee652
scholtm19 <scholtm19@ad.hva.nl>
2025-03-08 21:45:52 +0100

    UPDATE

--- server.js
+++ server.js
@@ -2,18 +2,20 @@ const { name } = require('ejs');
 const express= require('express');
 const path= require('path');
 const app = express();
-const port= 8000;
+const port= 5500;
 const file= path.resolve(__dirname, 'index.html');
 
 
 app.use('/static', express.static('static'));
 app.set('view engine', 'ejs');
-app.set('views', view);
+app.set('views', 'view');
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-app.get('/project', project);
+app.get('/project', (req, res) => {
+    res.render('index.ejs');
+});
 
 app.get('/', (req, res) => {
     res.sendFile(file);
