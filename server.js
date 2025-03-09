493a8d043002ddef91e186af64a8db08dcda2821
scholtm19 <scholtm19@ad.hva.nl>
2025-03-09 12:56:30 +0100

    back to html

--- server.js
+++ server.js
@@ -29,40 +29,12 @@
 //     res.render('index.ejs');
 // }
 
-// const express = require('express');
-// const path = require('path');
-// const app = express();
-// const port = 5500;
-
-
-
-// // Serve static files from the "public" directory
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// // Route for the home page
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'index.html'));
-// });
-
-// // Route for project pages
-// app.get('/project/:projectName', (req, res) => {
-//   const projectName = req.params.projectName;
-//   res.sendFile(path.join(__dirname, 'projects', `${projectName}.ejs`));
-// });
-
-// // Start the server
-// app.listen(port, '0.0.0.0', () => {
-//   console.log(`Server is running at http://185.199.108.153:${port}`);
-// });
-
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 5501;
+const port = 5500;
 
-// Set EJS as the template engine
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+
 
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
@@ -75,10 +47,38 @@ app.get('/', (req, res) => {
 // Route for project pages
 app.get('/project/:projectName', (req, res) => {
   const projectName = req.params.projectName;
-  res.render(projectName);
+  res.sendFile(path.join(__dirname, 'projects', `${projectName}.ejs`));
 });
 
 // Start the server
 app.listen(port, '0.0.0.0', () => {
   console.log(`Server is running at http://185.199.108.153:${port}`);
 });
+
+// const express = require('express');
+// const path = require('path');
+// const app = express();
+// const port = 5501;
+
+// // Set EJS as the template engine
+// app.set('view engine', 'ejs');
+// app.set('views', 'views');
+
+// // Serve static files from the "public" directory
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// // Route for the home page
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'index.html'));
+// });
+
+// // Route for project pages
+// app.get('/project/:projectName', (req, res) => {
+//   const projectName = req.params.projectName;
+//   res.render(projectName);
+// });
+
+// // Start the server
+// app.listen(port, '0.0.0.0', () => {
+//   console.log(`Server is running at http://185.199.108.153:${port}`);
+// });
